16a2de60ff201003ee4460aceaf8e1e2817aabc3
Brett Curtis <brett@osinfra.io>
2025-02-15 18:44:47 -0500

    Update pre-commit configuration and Datadog module source reference

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ provider "datadog" {
 # https://github.com/osinfra-io/terraform-datadog-google-integration
 
 module "datadog" {
-  source = "github.com/osinfra-io/terraform-datadog-google-integration?ref=resource-changes"
+  source = "github.com/osinfra-io/terraform-datadog-google-integration?ref=v0.3.4"
   count  = var.datadog_enable ? 1 : 0
 
   api_key                      = var.datadog_api_key
